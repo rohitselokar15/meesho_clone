6b0862d9537d440f243c67a357b9ea2a0b59127d
rohit-selokar <rohit.selokar@qloron.com>
2024-05-20 12:06:25 +0530

    product Detail page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import Header from './components/Header';
-import Home from './components/Home';
+import React from "react";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import {Routes, Route} from "react-router-dom";
+import ProductDetail from "./components/ProductDetail";
 
 const App = () => {
-    return (
-        <>
-            <Header/>
-            <Home/>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<ProductDetail/>}/>
+      </Routes>
+    </>
+  );
 };
 
 export default App;
